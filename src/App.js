0d622de2715456a4be6b0c3b4c3f439ccf21f554
Anish Srivastava <as0160895@gmail.com>
2024-09-25 00:41:53 +0530

    reactstrap and bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './component/Header';
-
+import { Button } from 'reactstrap';
 function App() {
   return (
     <div>
     <Header name="anish"/>
+    <Button>Click me pls</Button>
     <h2>This is app compnent</h2></div>
   );
 }
