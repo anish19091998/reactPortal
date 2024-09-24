dde95eca434f3c00b4949f6eb12edf811795c4d7
Anish Srivastava <as0160895@gmail.com>
2024-09-25 01:16:30 +0530

    reactstrap toastify is added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './component/Header';
 import { Button } from 'reactstrap';
+import Home from './component/Home';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
 function App() {
+  const notify = () => toast("Wow so easy!");
+ 
   return (
     <div>
+    
     <Header name="anish"/>
-    <Button>Click me pls</Button>
+    <Home/>
+
+    <Button color='warning'  onClick={notify}>Click me pls</Button>
+   
+
+
     <h2>This is app compnent</h2></div>
   );
 }
